2a2368462ac4a445c0c0e683edf7eb2af16b0ef6
Emily Wolin <ewolin@usgs.gov>
2022-05-24 17:37:51 -0600

    refactor to use functions from sisutils. also use datetimes for cleaner and more consistent calculation of battery age

--- battplot.py
+++ battplot.py
@@ -2,6 +2,13 @@
 # plot histograms of battery age
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime as dt
+
+
+# NOTE May 20 2022:
+# this script still relies on the existence of n4batts.csv
+# consider factoring out the part of battery2kml that calls the SIS API
+# and use that for input to this script as well?
 
 
 # IMPORTANT NOTE: we use 'ondate' for now (Epoch start date) instead of install date
@@ -13,12 +20,26 @@ import matplotlib.pyplot as plt
 # In general it is more correct to use the installdate, but this works for now!
 # (If we decided to start using epoch on dates to track when the batteries were purchased, rather than when they were installed, we'd definitely want to use install dates.)
 
-df = pd.read_csv('n4batts.csv', dtype={'lookupcode':'str'})
-#df = pd.read_csv('all_asl_batts.csv', dtype={'lookupcode':'str'})
+from sisutils import loginSIS, getEquipmentInstallations, extractBatteryInfo
+
+
+# Send request to SIS API and parse returned text
+token = loginSIS()
+
+requesttext = getEquipmentInstallations(token, category="Battery", netcode="N4", isactive="y", pagesize="10000", sort="ondate")
+
+outfile = extractBatteryInfo(requesttext)
+
+#df = pd.read_csv('n4batts.csv', dtype={'lookupcode':'str'})
+df = pd.read_csv(outfile, dtype={'lookupcode':'str'})
 df.sort_values(by='ondate')
-df ['ondate'] = pd.to_datetime(df['ondate'], format='%Y-%m-%d (%j) %H:%M:%S')
+
+#dateformat = "%Y-%m-%d (%j) %H:%M:%S" # format if using CSV downloaded from SIS search results page
+dateformat = "%Y-%m-%dT%H:%M:%SZ" # format for results written from SIS API without applying any date formatting
+df ['ondate'] = pd.to_datetime(df['ondate'], format=dateformat)
 
 
+# Some simple plots:
 # plot individual batteries
 fig, ax = plt.subplots()
 df['ondate'].groupby(df["ondate"].dt.year).count().plot(kind='bar', ax=ax)
@@ -26,6 +47,7 @@ ax.set_ylabel('# of batteries')
 ax.set_xlabel('last battery swap')
 plt.tight_layout()
 fig.savefig('allbatts.png')
+print('saved allbatts.png')
 
 # group by station
 df2 = df[['lookupcode', 'ondate']]
@@ -37,41 +59,44 @@ ax.set_ylabel('# of stations')
 ax.set_xlabel('last battery swap')
 plt.tight_layout()
 fig.savefig('n4batts_bystn.png')
+print('saved n4batts_bystn.png')
 
+# Make fancier plot: histogram showing year of last battery swap at each station
 fig, ax = plt.subplots()
 
 
 print('------')
-for f in df3.groupby(df3["ondate"].dt.year):
-    print(f[1].reset_index(drop=True, inplace=True))
-    print(f[0])
+year_now = dt.datetime.utcnow()
+year_int = int(dt.datetime.utcnow().strftime("%Y"))
+for install_year, battlist in df3.groupby(df3["ondate"].dt.year):
+    print(battlist.reset_index(drop=True, inplace=True))
+    print(install_year)
     color='black'
-    yr=2022
-    for i in range(len(f[1])): 
-        if f[0] <= yr-7:
+    for i in range(len(battlist)): 
+        install_dt = battlist.iloc[i]['ondate']
+        battery_age = year_now - battlist.iloc[i]['ondate']
+        battery_age_yr = battery_age.days/365
+        if battery_age_yr >= 7:
             color='red'
-        elif f[0] < yr-5 and f[0] > yr-7:
+        elif battery_age_yr > 5 and battery_age_yr < 7:
             color='orange'
         else:
             color='green'
-        ax.text(f[0], i, f[1].iloc[i]["lookupcode"], ha='center', va='bottom', color=color) 
-#        print(ax.text(f[0], i, f[1].iloc[i]["lookupcode"])) 
+        ax.text(install_year, i, battlist.iloc[i]["lookupcode"], ha='center', va='bottom', color=color) 
 
-ax.fill_between([2008,2023],[20,20],[30,30], color='grey', alpha=0.5)
 
-ax.set_xlim(2008,2023)
-#ax.set_ylim(0,35)
-#ax.set_ylim(0,45)
+min_year = df3['ondate'].min().year
+
+
+# Average number of batteries needed to replace each year: between 20 and 30
+
+ax.fill_between([min_year,year_int+1],[20,20],[30,30], color='grey', alpha=0.1)
+
+ax.set_xlim(min_year-1,year_int+1)
 ax.set_ylim(0,65)
 ax.set_ylabel('# of stations')
 ax.set_xlabel('last battery swap')
 fig.set_size_inches(7,8)
 plt.tight_layout()
 fig.savefig('text.png')
-
-
-
-for i,s in enumerate(df['lookupcode'].drop_duplicates()[:]): 
-    print(i,s) 
-
-
+print('saved text.png')
